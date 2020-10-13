0ef890688a0ab399c6ee07f21ce757a7895b4019
Justine Hoffa <justinefarrer@yahoo.com>
2020-10-13 08:45:02 -0600

    Add project files.

---
+++ CarRentalForm.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CarRentalForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CustomerNameTextBox = New System.Windows.Forms.TextBox()
+        Me.AddressTextBox = New System.Windows.Forms.TextBox()
+        Me.CityTextBox = New System.Windows.Forms.TextBox()
+        Me.StateTextBox = New System.Windows.Forms.TextBox()
+        Me.ZipCodeTextBox = New System.Windows.Forms.TextBox()
+        Me.BeginningOdometerReadingTextBox = New System.Windows.Forms.TextBox()
+        Me.EndingOdometerReadingTextBox = New System.Windows.Forms.TextBox()
+        Me.NumberOfDaysTextBox = New System.Windows.Forms.TextBox()
+        Me.CustomerNameLabel = New System.Windows.Forms.Label()
+        Me.AddressLabel = New System.Windows.Forms.Label()
+        Me.CityLabel = New System.Windows.Forms.Label()
+        Me.StateLabel = New System.Windows.Forms.Label()
+        Me.ZipCodeLabel = New System.Windows.Forms.Label()
+        Me.BeginningOdometerReadingLabel = New System.Windows.Forms.Label()
+        Me.EndingOdometerReadingLabel = New System.Windows.Forms.Label()
+        Me.NumberOfDaysLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'CustomerNameTextBox
+        '
+        Me.CustomerNameTextBox.Location = New System.Drawing.Point(349, 26)
+        Me.CustomerNameTextBox.Name = "CustomerNameTextBox"
+        Me.CustomerNameTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.CustomerNameTextBox.TabIndex = 0
+        '
+        'AddressTextBox
+        '
+        Me.AddressTextBox.Location = New System.Drawing.Point(349, 81)
+        Me.AddressTextBox.Name = "AddressTextBox"
+        Me.AddressTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.AddressTextBox.TabIndex = 1
+        '
+        'CityTextBox
+        '
+        Me.CityTextBox.Location = New System.Drawing.Point(349, 140)
+        Me.CityTextBox.Name = "CityTextBox"
+        Me.CityTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.CityTextBox.TabIndex = 2
+        '
+        'StateTextBox
+        '
+        Me.StateTextBox.Location = New System.Drawing.Point(349, 193)
+        Me.StateTextBox.Name = "StateTextBox"
+        Me.StateTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.StateTextBox.TabIndex = 3
+        '
+        'ZipCodeTextBox
+        '
+        Me.ZipCodeTextBox.Location = New System.Drawing.Point(349, 251)
+        Me.ZipCodeTextBox.Name = "ZipCodeTextBox"
+        Me.ZipCodeTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.ZipCodeTextBox.TabIndex = 4
+        '
+        'BeginningOdometerReadingTextBox
+        '
+        Me.BeginningOdometerReadingTextBox.Location = New System.Drawing.Point(349, 309)
+        Me.BeginningOdometerReadingTextBox.Name = "BeginningOdometerReadingTextBox"
+        Me.BeginningOdometerReadingTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.BeginningOdometerReadingTextBox.TabIndex = 5
+        '
+        'EndingOdometerReadingTextBox
+        '
+        Me.EndingOdometerReadingTextBox.Location = New System.Drawing.Point(349, 365)
+        Me.EndingOdometerReadingTextBox.Name = "EndingOdometerReadingTextBox"
+        Me.EndingOdometerReadingTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.EndingOdometerReadingTextBox.TabIndex = 6
+        '
+        'NumberOfDaysTextBox
+        '
+        Me.NumberOfDaysTextBox.Location = New System.Drawing.Point(349, 420)
+        Me.NumberOfDaysTextBox.Name = "NumberOfDaysTextBox"
+        Me.NumberOfDaysTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.NumberOfDaysTextBox.TabIndex = 7
+        '
+        'CustomerNameLabel
+        '
+        Me.CustomerNameLabel.AutoSize = True
+        Me.CustomerNameLabel.Location = New System.Drawing.Point(31, 29)
+        Me.CustomerNameLabel.Name = "CustomerNameLabel"
+        Me.CustomerNameLabel.Size = New System.Drawing.Size(166, 25)
+        Me.CustomerNameLabel.TabIndex = 8
+        Me.CustomerNameLabel.Text = "Customer Name"
+        '
+        'AddressLabel
+        '
+        Me.AddressLabel.AutoSize = True
+        Me.AddressLabel.Location = New System.Drawing.Point(22, 84)
+        Me.AddressLabel.Name = "AddressLabel"
+        Me.AddressLabel.Size = New System.Drawing.Size(91, 25)
+        Me.AddressLabel.TabIndex = 9
+        Me.AddressLabel.Text = "Address"
+        '
+        'CityLabel
+        '
+        Me.CityLabel.AutoSize = True
+        Me.CityLabel.Location = New System.Drawing.Point(22, 143)
+        Me.CityLabel.Name = "CityLabel"
+        Me.CityLabel.Size = New System.Drawing.Size(49, 25)
+        Me.CityLabel.TabIndex = 10
+        Me.CityLabel.Text = "City"
+        '
+        'StateLabel
+        '
+        Me.StateLabel.AutoSize = True
+        Me.StateLabel.Location = New System.Drawing.Point(22, 196)
+        Me.StateLabel.Name = "StateLabel"
+        Me.StateLabel.Size = New System.Drawing.Size(62, 25)
+        Me.StateLabel.TabIndex = 11
+        Me.StateLabel.Text = "State"
+        '
+        'ZipCodeLabel
+        '
+        Me.ZipCodeLabel.AutoSize = True
+        Me.ZipCodeLabel.Location = New System.Drawing.Point(22, 254)
+        Me.ZipCodeLabel.Name = "ZipCodeLabel"
+        Me.ZipCodeLabel.Size = New System.Drawing.Size(99, 25)
+        Me.ZipCodeLabel.TabIndex = 12
+        Me.ZipCodeLabel.Text = "Zip Code"
+        '
+        'BeginningOdometerReadingLabel
+        '
+        Me.BeginningOdometerReadingLabel.AutoSize = True
+        Me.BeginningOdometerReadingLabel.Location = New System.Drawing.Point(22, 312)
+        Me.BeginningOdometerReadingLabel.Name = "BeginningOdometerReadingLabel"
+        Me.BeginningOdometerReadingLabel.Size = New System.Drawing.Size(294, 25)
+        Me.BeginningOdometerReadingLabel.TabIndex = 13
+        Me.BeginningOdometerReadingLabel.Text = "Beginning Odometer Reading"
+        '
+        'EndingOdometerReadingLabel
+        '
+        Me.EndingOdometerReadingLabel.AutoSize = True
+        Me.EndingOdometerReadingLabel.Location = New System.Drawing.Point(22, 368)
+        Me.EndingOdometerReadingLabel.Name = "EndingOdometerReadingLabel"
+        Me.EndingOdometerReadingLabel.Size = New System.Drawing.Size(265, 25)
+        Me.EndingOdometerReadingLabel.TabIndex = 14
+        Me.EndingOdometerReadingLabel.Text = "Ending Odometer Reading"
+        '
+        'NumberOfDaysLabel
+        '
+        Me.NumberOfDaysLabel.AutoSize = True
+        Me.NumberOfDaysLabel.Location = New System.Drawing.Point(22, 423)
+        Me.NumberOfDaysLabel.Name = "NumberOfDaysLabel"
+        Me.NumberOfDaysLabel.Size = New System.Drawing.Size(166, 25)
+        Me.NumberOfDaysLabel.TabIndex = 15
+        Me.NumberOfDaysLabel.Text = "Number of Days"
+        '
+        'CarRentalForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1268, 888)
+        Me.Controls.Add(Me.NumberOfDaysLabel)
+        Me.Controls.Add(Me.EndingOdometerReadingLabel)
+        Me.Controls.Add(Me.BeginningOdometerReadingLabel)
+        Me.Controls.Add(Me.ZipCodeLabel)
+        Me.Controls.Add(Me.StateLabel)
+        Me.Controls.Add(Me.CityLabel)
+        Me.Controls.Add(Me.AddressLabel)
+        Me.Controls.Add(Me.CustomerNameLabel)
+        Me.Controls.Add(Me.NumberOfDaysTextBox)
+        Me.Controls.Add(Me.EndingOdometerReadingTextBox)
+        Me.Controls.Add(Me.BeginningOdometerReadingTextBox)
+        Me.Controls.Add(Me.ZipCodeTextBox)
+        Me.Controls.Add(Me.StateTextBox)
+        Me.Controls.Add(Me.CityTextBox)
+        Me.Controls.Add(Me.AddressTextBox)
+        Me.Controls.Add(Me.CustomerNameTextBox)
+        Me.Name = "CarRentalForm"
+        Me.Text = "Acme Car Rental Service"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CustomerNameTextBox As TextBox
+    Friend WithEvents AddressTextBox As TextBox
+    Friend WithEvents CityTextBox As TextBox
+    Friend WithEvents StateTextBox As TextBox
+    Friend WithEvents ZipCodeTextBox As TextBox
+    Friend WithEvents BeginningOdometerReadingTextBox As TextBox
+    Friend WithEvents EndingOdometerReadingTextBox As TextBox
+    Friend WithEvents NumberOfDaysTextBox As TextBox
+    Friend WithEvents CustomerNameLabel As Label
+    Friend WithEvents AddressLabel As Label
+    Friend WithEvents CityLabel As Label
+    Friend WithEvents StateLabel As Label
+    Friend WithEvents ZipCodeLabel As Label
+    Friend WithEvents BeginningOdometerReadingLabel As Label
+    Friend WithEvents EndingOdometerReadingLabel As Label
+    Friend WithEvents NumberOfDaysLabel As Label
+End Class
---
+++ CarRentalForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class CarRentalForm
+
+End Class
