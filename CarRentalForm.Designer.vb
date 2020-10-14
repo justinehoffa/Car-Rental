3614c529d423b6832e849d97a6c084a797bd5d00
Justine Hoffa <justinefarrer@yahoo.com>
2020-10-14 08:45:41 -0600

    start code

--- CarRentalForm.Designer.vb
+++ CarRentalForm.Designer.vb
@@ -38,6 +38,28 @@ Partial Class CarRentalForm
         Me.BeginningOdometerReadingLabel = New System.Windows.Forms.Label()
         Me.EndingOdometerReadingLabel = New System.Windows.Forms.Label()
         Me.NumberOfDaysLabel = New System.Windows.Forms.Label()
+        Me.OdometerGroupBox = New System.Windows.Forms.GroupBox()
+        Me.DiscountsGroupBox = New System.Windows.Forms.GroupBox()
+        Me.MilesRadioButton = New System.Windows.Forms.RadioButton()
+        Me.KilometersRadioButton = New System.Windows.Forms.RadioButton()
+        Me.AAAMemberDiscountCheckBox = New System.Windows.Forms.CheckBox()
+        Me.SeniorCitizenDiscountCheckBox = New System.Windows.Forms.CheckBox()
+        Me.DistanceTraveledinMilesTextBox = New System.Windows.Forms.TextBox()
+        Me.MileageChargeTextBox = New System.Windows.Forms.TextBox()
+        Me.DayChargeTextBox = New System.Windows.Forms.TextBox()
+        Me.MinusDiscount = New System.Windows.Forms.TextBox()
+        Me.AmountOwedTextBox = New System.Windows.Forms.TextBox()
+        Me.DistanceTraveledinMilesLabel = New System.Windows.Forms.Label()
+        Me.MileageChargeLabel = New System.Windows.Forms.Label()
+        Me.DayChargeLabel = New System.Windows.Forms.Label()
+        Me.MinusDiscountLabel = New System.Windows.Forms.Label()
+        Me.AmountOwedLabel = New System.Windows.Forms.Label()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SummaryButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.OdometerGroupBox.SuspendLayout()
+        Me.DiscountsGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'CustomerNameTextBox
@@ -168,11 +190,210 @@ Partial Class CarRentalForm
         Me.NumberOfDaysLabel.TabIndex = 15
         Me.NumberOfDaysLabel.Text = "Number of Days"
         '
+        'OdometerGroupBox
+        '
+        Me.OdometerGroupBox.Controls.Add(Me.KilometersRadioButton)
+        Me.OdometerGroupBox.Controls.Add(Me.MilesRadioButton)
+        Me.OdometerGroupBox.Location = New System.Drawing.Point(132, 530)
+        Me.OdometerGroupBox.Name = "OdometerGroupBox"
+        Me.OdometerGroupBox.Size = New System.Drawing.Size(435, 245)
+        Me.OdometerGroupBox.TabIndex = 16
+        Me.OdometerGroupBox.TabStop = False
+        Me.OdometerGroupBox.Text = "Is Odometer in Miles or Kilometers?"
+        '
+        'DiscountsGroupBox
+        '
+        Me.DiscountsGroupBox.Controls.Add(Me.SeniorCitizenDiscountCheckBox)
+        Me.DiscountsGroupBox.Controls.Add(Me.AAAMemberDiscountCheckBox)
+        Me.DiscountsGroupBox.Location = New System.Drawing.Point(632, 530)
+        Me.DiscountsGroupBox.Name = "DiscountsGroupBox"
+        Me.DiscountsGroupBox.Size = New System.Drawing.Size(435, 246)
+        Me.DiscountsGroupBox.TabIndex = 17
+        Me.DiscountsGroupBox.TabStop = False
+        Me.DiscountsGroupBox.Text = "Enter any Discounts"
+        '
+        'MilesRadioButton
+        '
+        Me.MilesRadioButton.AutoSize = True
+        Me.MilesRadioButton.Location = New System.Drawing.Point(100, 62)
+        Me.MilesRadioButton.Name = "MilesRadioButton"
+        Me.MilesRadioButton.Size = New System.Drawing.Size(94, 29)
+        Me.MilesRadioButton.TabIndex = 0
+        Me.MilesRadioButton.TabStop = True
+        Me.MilesRadioButton.Text = "Miles"
+        Me.MilesRadioButton.UseVisualStyleBackColor = True
+        '
+        'KilometersRadioButton
+        '
+        Me.KilometersRadioButton.AutoSize = True
+        Me.KilometersRadioButton.Location = New System.Drawing.Point(100, 144)
+        Me.KilometersRadioButton.Name = "KilometersRadioButton"
+        Me.KilometersRadioButton.Size = New System.Drawing.Size(144, 29)
+        Me.KilometersRadioButton.TabIndex = 1
+        Me.KilometersRadioButton.TabStop = True
+        Me.KilometersRadioButton.Text = "Kilometers"
+        Me.KilometersRadioButton.UseVisualStyleBackColor = True
+        '
+        'AAAMemberDiscountCheckBox
+        '
+        Me.AAAMemberDiscountCheckBox.AutoSize = True
+        Me.AAAMemberDiscountCheckBox.Location = New System.Drawing.Point(117, 63)
+        Me.AAAMemberDiscountCheckBox.Name = "AAAMemberDiscountCheckBox"
+        Me.AAAMemberDiscountCheckBox.Size = New System.Drawing.Size(260, 29)
+        Me.AAAMemberDiscountCheckBox.TabIndex = 2
+        Me.AAAMemberDiscountCheckBox.Text = "AAA Member Discount"
+        Me.AAAMemberDiscountCheckBox.UseVisualStyleBackColor = True
+        '
+        'SeniorCitizenDiscountCheckBox
+        '
+        Me.SeniorCitizenDiscountCheckBox.AutoSize = True
+        Me.SeniorCitizenDiscountCheckBox.Location = New System.Drawing.Point(117, 144)
+        Me.SeniorCitizenDiscountCheckBox.Name = "SeniorCitizenDiscountCheckBox"
+        Me.SeniorCitizenDiscountCheckBox.Size = New System.Drawing.Size(268, 29)
+        Me.SeniorCitizenDiscountCheckBox.TabIndex = 3
+        Me.SeniorCitizenDiscountCheckBox.Text = "Senior Citizen Discount"
+        Me.SeniorCitizenDiscountCheckBox.UseVisualStyleBackColor = True
+        '
+        'DistanceTraveledinMilesTextBox
+        '
+        Me.DistanceTraveledinMilesTextBox.Location = New System.Drawing.Point(1109, 26)
+        Me.DistanceTraveledinMilesTextBox.Name = "DistanceTraveledinMilesTextBox"
+        Me.DistanceTraveledinMilesTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.DistanceTraveledinMilesTextBox.TabIndex = 18
+        '
+        'MileageChargeTextBox
+        '
+        Me.MileageChargeTextBox.Location = New System.Drawing.Point(1109, 81)
+        Me.MileageChargeTextBox.Name = "MileageChargeTextBox"
+        Me.MileageChargeTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.MileageChargeTextBox.TabIndex = 19
+        '
+        'DayChargeTextBox
+        '
+        Me.DayChargeTextBox.Location = New System.Drawing.Point(1109, 140)
+        Me.DayChargeTextBox.Name = "DayChargeTextBox"
+        Me.DayChargeTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.DayChargeTextBox.TabIndex = 20
+        '
+        'MinusDiscount
+        '
+        Me.MinusDiscount.Location = New System.Drawing.Point(1109, 193)
+        Me.MinusDiscount.Name = "MinusDiscount"
+        Me.MinusDiscount.Size = New System.Drawing.Size(310, 31)
+        Me.MinusDiscount.TabIndex = 21
+        '
+        'AmountOwedTextBox
+        '
+        Me.AmountOwedTextBox.Location = New System.Drawing.Point(1109, 251)
+        Me.AmountOwedTextBox.Name = "AmountOwedTextBox"
+        Me.AmountOwedTextBox.Size = New System.Drawing.Size(310, 31)
+        Me.AmountOwedTextBox.TabIndex = 22
+        '
+        'DistanceTraveledinMilesLabel
+        '
+        Me.DistanceTraveledinMilesLabel.AutoSize = True
+        Me.DistanceTraveledinMilesLabel.Location = New System.Drawing.Point(785, 32)
+        Me.DistanceTraveledinMilesLabel.Name = "DistanceTraveledinMilesLabel"
+        Me.DistanceTraveledinMilesLabel.Size = New System.Drawing.Size(266, 25)
+        Me.DistanceTraveledinMilesLabel.TabIndex = 23
+        Me.DistanceTraveledinMilesLabel.Text = "Distance Traveled in Miles"
+        '
+        'MileageChargeLabel
+        '
+        Me.MileageChargeLabel.AutoSize = True
+        Me.MileageChargeLabel.Location = New System.Drawing.Point(870, 84)
+        Me.MileageChargeLabel.Name = "MileageChargeLabel"
+        Me.MileageChargeLabel.Size = New System.Drawing.Size(164, 25)
+        Me.MileageChargeLabel.TabIndex = 24
+        Me.MileageChargeLabel.Text = "Mileage Charge"
+        '
+        'DayChargeLabel
+        '
+        Me.DayChargeLabel.AutoSize = True
+        Me.DayChargeLabel.Location = New System.Drawing.Point(870, 146)
+        Me.DayChargeLabel.Name = "DayChargeLabel"
+        Me.DayChargeLabel.Size = New System.Drawing.Size(126, 25)
+        Me.DayChargeLabel.TabIndex = 25
+        Me.DayChargeLabel.Text = "Day Charge"
+        '
+        'MinusDiscountLabel
+        '
+        Me.MinusDiscountLabel.AutoSize = True
+        Me.MinusDiscountLabel.Location = New System.Drawing.Point(870, 199)
+        Me.MinusDiscountLabel.Name = "MinusDiscountLabel"
+        Me.MinusDiscountLabel.Size = New System.Drawing.Size(160, 25)
+        Me.MinusDiscountLabel.TabIndex = 26
+        Me.MinusDiscountLabel.Text = "Minus Discount"
+        '
+        'AmountOwedLabel
+        '
+        Me.AmountOwedLabel.AutoSize = True
+        Me.AmountOwedLabel.Location = New System.Drawing.Point(870, 257)
+        Me.AmountOwedLabel.Name = "AmountOwedLabel"
+        Me.AmountOwedLabel.Size = New System.Drawing.Size(146, 25)
+        Me.AmountOwedLabel.TabIndex = 27
+        Me.AmountOwedLabel.Text = "Amount Owed"
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Location = New System.Drawing.Point(132, 801)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(199, 94)
+        Me.CalculateButton.TabIndex = 28
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(384, 801)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(199, 94)
+        Me.ClearButton.TabIndex = 29
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SummaryButton
+        '
+        Me.SummaryButton.Location = New System.Drawing.Point(641, 801)
+        Me.SummaryButton.Name = "SummaryButton"
+        Me.SummaryButton.Size = New System.Drawing.Size(199, 94)
+        Me.SummaryButton.TabIndex = 30
+        Me.SummaryButton.Text = "Summary"
+        Me.SummaryButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(899, 801)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(199, 94)
+        Me.ExitButton.TabIndex = 31
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
         'CarRentalForm
         '
+        Me.AcceptButton = Me.CalculateButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1268, 888)
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(1450, 922)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SummaryButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.AmountOwedLabel)
+        Me.Controls.Add(Me.MinusDiscountLabel)
+        Me.Controls.Add(Me.DayChargeLabel)
+        Me.Controls.Add(Me.MileageChargeLabel)
+        Me.Controls.Add(Me.DistanceTraveledinMilesLabel)
+        Me.Controls.Add(Me.AmountOwedTextBox)
+        Me.Controls.Add(Me.MinusDiscount)
+        Me.Controls.Add(Me.DayChargeTextBox)
+        Me.Controls.Add(Me.MileageChargeTextBox)
+        Me.Controls.Add(Me.DistanceTraveledinMilesTextBox)
+        Me.Controls.Add(Me.DiscountsGroupBox)
+        Me.Controls.Add(Me.OdometerGroupBox)
         Me.Controls.Add(Me.NumberOfDaysLabel)
         Me.Controls.Add(Me.EndingOdometerReadingLabel)
         Me.Controls.Add(Me.BeginningOdometerReadingLabel)
@@ -191,6 +412,10 @@ Partial Class CarRentalForm
         Me.Controls.Add(Me.CustomerNameTextBox)
         Me.Name = "CarRentalForm"
         Me.Text = "Acme Car Rental Service"
+        Me.OdometerGroupBox.ResumeLayout(False)
+        Me.OdometerGroupBox.PerformLayout()
+        Me.DiscountsGroupBox.ResumeLayout(False)
+        Me.DiscountsGroupBox.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -212,4 +437,24 @@ Partial Class CarRentalForm
     Friend WithEvents BeginningOdometerReadingLabel As Label
     Friend WithEvents EndingOdometerReadingLabel As Label
     Friend WithEvents NumberOfDaysLabel As Label
+    Friend WithEvents OdometerGroupBox As GroupBox
+    Friend WithEvents KilometersRadioButton As RadioButton
+    Friend WithEvents MilesRadioButton As RadioButton
+    Friend WithEvents DiscountsGroupBox As GroupBox
+    Friend WithEvents SeniorCitizenDiscountCheckBox As CheckBox
+    Friend WithEvents AAAMemberDiscountCheckBox As CheckBox
+    Friend WithEvents DistanceTraveledinMilesTextBox As TextBox
+    Friend WithEvents MileageChargeTextBox As TextBox
+    Friend WithEvents DayChargeTextBox As TextBox
+    Friend WithEvents MinusDiscount As TextBox
+    Friend WithEvents AmountOwedTextBox As TextBox
+    Friend WithEvents DistanceTraveledinMilesLabel As Label
+    Friend WithEvents MileageChargeLabel As Label
+    Friend WithEvents DayChargeLabel As Label
+    Friend WithEvents MinusDiscountLabel As Label
+    Friend WithEvents AmountOwedLabel As Label
+    Friend WithEvents CalculateButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents SummaryButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- CarRentalForm.vb
+++ CarRentalForm.vb
@@ -1,3 +1,13 @@
-﻿Public Class CarRentalForm
+﻿'Justine Hoffa
+'RCET0265
+'Fall2020
+'Car Rental
+'
 
+
+
+Public Class CarRentalForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 End Class
